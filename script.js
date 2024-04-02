161ab37b6bc001201f61c8ea26747825b53df9d4
Vlorzy <84677278+Vlorzy@users.noreply.github.com>
2024-04-02 00:26:38 -0300

    hola no se q poner me da paja

--- script.js
+++ script.js
@@ -92,3 +92,23 @@ typeText();
 // *TRANSLATOR* //   });
 // *TRANSLATOR* // });
   
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  
+    window.addEventListener('scroll', function() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+      } else {
+        scrollToTopBtn.style.display = "none";
+      }
+    });
+  
+    scrollToTopBtn.addEventListener('click', scrollToTop);
+  });
